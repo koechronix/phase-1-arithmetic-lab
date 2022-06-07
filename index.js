cefaa3c79ecf815c85d667fb9d6d518de830c35a
Amos Koech <koechronix@yahoo.com>
2022-06-07 06:00:57 -0400

    completed arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2
+const multiply = num1 * num2 ;
+
+const random = Math.floor(Math.random() *105 ) +1 ;
+
+let num3 = 12;
+let num4 = 8;
+const mod = (12 % 8);
+
+const max = (4,8,15,20);
+
